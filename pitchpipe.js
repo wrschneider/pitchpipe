38f5abf002f9fe07531aefd31ffd18e53e840d37
Schneider, Bill <SchneidB@advisory.com>
2014-05-11 07:20:50 -0400

    first revision

---
+++ pitchpipe.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+   var AudioContext = window.AudioContext || window.webkitAudioContext;
+   var audioContext = new AudioContext();
+   var frequencies = [349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.25, 698.46];
+   var currOscillator;
+   
+   var playNote = function playNote(freq, duration) {
+	 if (currOscillator) {
+	   currOscillator.noteOff(0);
+	 }
+     var osc = audioContext.createOscillator();
+	 osc.frequency.value = freq;
+	 osc.type = 1;
+	 osc.connect(audioContext.destination);
+	 osc.noteOn(0);
+	 setTimeout(function() { osc.noteOff(0); }, duration);
+	 currOscillator = osc;
+   }
+   
+   $("button.noteButton").click(function() {
+     var noteId = $(this).data('buttonIndex');
+	 playNote(frequencies[noteId], 3000);
+   })
+});
